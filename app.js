97845d4d5ea6ed50e539bc3e69978e91b7eeca23
Wiggins <wwiggins1@una.edu>
2023-08-29 13:11:32 -0500

    updating to move computers

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express()
+const port = (process.env.PORT || 3000)
 
 app.get('/', function (req, res) {
   res.send('<h1>Hello World From Express & a Paas/Render (and Will)</h1>')
 })
 
-app.listen(3000)
+app.get('/index', function (req, res) {
+  res.sendFile('index.html');
+})
+
+
+// app.listen(3000)
+
+app.listen(port, () => console.log(`Server is running on...on ${ port }`));
