7d03580bc6c99b680fee28ad02d2544e36c12f0c
Wiggins <wwiggins1@una.edu>
2023-08-29 13:36:28 -0500

    mike_November, aight?

--- app.js
+++ app.js
@@ -2,13 +2,24 @@ const express = require('express')
 const app = express()
 const port = (process.env.PORT || 3000)
 
+app.set('view engine', 'ejs');
+
+let myVariableServer = 'soft coded server data'
+
+app.get('/views', function (req, res) {
+  res.render('index', {
+    'myVariableClient' : myVariableServer
+  });
+})
+
 app.get('/', function (req, res) {
   res.send('<h1>Hello World From Express & a Paas/Render (and Will)</h1>')
 })
 
 app.get('/index', function (req, res) {
-  res.sendFile(__dirname + 'index.html');
+  res.sendFile(__dirname + '/index.html');
 })
+
 
 
 // app.listen(3000)
