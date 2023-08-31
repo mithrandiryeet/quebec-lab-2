b8f920900aa4e0dc9d133b1f81a1cdfd7336e041
Wiggins <wwiggins1@una.edu>
2023-08-31 13:32:22 -0500

    this does not work

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ app.set('view engine', 'ejs');
 let myVariableServer = 'soft coded server data'
 
 app.get('/views', function (req, res) {
-  res.render('index', {
+  res.render('index', 
+  {
     'myVariableClient' : myVariableServer
   });
 })
@@ -16,11 +17,23 @@ app.get('/', function (req, res) {
   res.send('<h1>Hello World From Express & a Paas/Render (and Will)</h1>')
 })
 
-app.get('/index', function (req, res) {
+app.get('/index', function (req, res) {        // '/index' can be called anything
   res.sendFile(__dirname + '/index.html');
 })
 
+app.post('/postClientData', function (req, res) {
 
+
+  console.log("body: ", req.body)
+  console.log("parmas: ", req.params['userName']);
+
+  myVariableServer = 'now we\'ve posted'
+
+  res.render('index', 
+  {
+    'myVariableClient' : myVariableServer
+  });
+})
 
 // app.listen(3000)
 
