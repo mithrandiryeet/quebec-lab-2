cc4df57de8b3fea22a64dac1913e6e980945399e
Will Wiggins <willswiggins@gmail.com>
2023-09-04 13:53:04 -0500

    this should work now

--- app.js
+++ app.js
@@ -1,40 +1,46 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const port = (process.env.PORT || 3000)
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 let myVariableServer = 'soft coded server data'
 
 app.get('/views', function (req, res) {
   res.render('index', 
   {
-    'myVariableClient' : myVariableServer
-  });
-})
-
-app.get('/', function (req, res) {
-  res.send('<h1>Hello World From Express & a Paas/Render (and Will)</h1>')
-})
-
-app.get('/index', function (req, res) {        // '/index' can be called anything
-  res.sendFile(__dirname + '/index.html');
+    'myVariableClient' : myVariableServer 
+  }
+  );
 })
 
 app.post('/postClientData', function (req, res) {
-
-
+  
   console.log("body: ", req.body)
-  console.log("parmas: ", req.params['userName']);
+  console.log("user Name: ", req.body.userName)
+ //  console.log("params: ", req.params['userName']);
+ 
+ // myVariableServer = req.body.userName;
 
-  myVariableServer = 'now we\'ve posted'
-
-  res.render('index', 
-  {
-    'myVariableClient' : myVariableServer
-  });
+ res.render('index', 
+ {
+   'myVariableClient' : req.body.userName 
+ }
+ );
 })
+
+app.get('/', function (req, res) {
+  res.send('<h1>Hello World From Express & a PaaS/Render</h1>')
+})
+
+app.get('/whatever', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+})
+
+
 
 // app.listen(3000)
 
-app.listen(port, () => console.log(`Server is running on... ${ port }`));
+app.listen(port, () => console.log(`Server is running...on ${ port }` ));
